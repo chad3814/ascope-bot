99b5d7aa74edc1a8409b828f3c9c2efa31443c7f
Jude Morrissey <judemorrissey@users.noreply.github.com>
2022-05-11 20:00:19 -0700

    fix index.js indents from mixed 2/4 spaces to 2 spaces

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { fileURLToPath } from 'url';
-import path, { resolve } from 'path';
-import { readFileSync } from 'fs';
+import {fileURLToPath} from 'url';
+import path from 'path';
+import {readFileSync} from 'fs';
 
 import bodyAny from 'body/any.js';
 import express from 'express';
@@ -10,66 +10,69 @@ const dirname = path.dirname(fileURLToPath(import.meta.url));
 const bot_token = readFileSync(path.join(dirname, 'slack_token.txt'), 'utf-8');
 
 const bodyParser = (req) => {
-    return new Promise((resolve, reject) => {
-        return bodyAny(req, (err, body) => {
-            if (err) {
-                return reject(err);
-            }
-            return resolve(body);
-        });
+  return new Promise((resolve, reject) => {
+    return bodyAny(req, (err, body) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(body);
     });
+  });
 };
 
 const sendToSlack = async (channel, text, extra = {}) => {
-    const body = JSON.stringify({
-        channel,
-        text,
-        ...extra,
-    });
-    const resp = await fetch('https://slack.com/api/chat.postMessage', {
-        headers:{
-            'Authorization': `Bearer ${bot_token}`,
-            'Content-Type': 'application/json',
-        },
-        method: 'POST',
-        body,
-    });
-    const json = await resp.json();
-    console.log('sendToSlack', json);
+  const body = JSON.stringify({
+    channel,
+    text,
+    ...extra,
+  });
+  const resp = await fetch('https://slack.com/api/chat.postMessage', {
+    body,
+    headers: {
+      'Authorization': `Bearer ${bot_token}`,
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+  });
+  const json = await resp.json();
+  console.log('sendToSlack', json);
 };
 
 const app = express();
 app.use((req, res, next) => {
-    console.log(req.method, req.url);
-    next();
+  console.log(req.method, req.url);
+  next();
 });
 app.use(express.static(dirname));
 
 app.post('/facepalm', async (req, res) => {
-    res.end();
-    const body = await bodyParser(req);
+  res.end();
+  const body = await bodyParser(req);
 
-    const blocks = [
-      {
-        type: 'image',
-        image_url:
-          'https://ascope.chadshost.xyz/images/chris-facepalm.png',
-        alt_text: 'facepalm',
-      },
-    ];
+  const blocks = [
+    {
+      alt_text: 'facepalm',
+      image_url: 'https://ascope.chadshost.xyz/images/chris-facepalm.png',
+      type: 'image',
+    },
+  ];
 
-    await sendToSlack(body.channel_id, 'facepalm', {
-        blocks,
-    });
+  await sendToSlack(body.channel_id, 'facepalm', {
+    blocks,
+  });
 });
 
 app.get('/wotd', async (req, res) => {
-    res.end('OK');
-    await sendToSlack('C02FSTKLP98', 'https://www.merriam-webster.com/word-of-the-day', {
-        username: 'Word of the Day',
-        unfurl_links: true,
-        icon_emoji: 'calendar',
-    });
+  res.end('OK');
+  await sendToSlack(
+    'C02FSTKLP98',
+    'https://www.merriam-webster.com/word-of-the-day',
+    {
+      icon_emoji: 'calendar',
+      unfurl_links: true,
+      username: 'Word of the Day',
+    },
+  );
 });
 
 const port = parseInt(process.argv[2], 10) || 80;
